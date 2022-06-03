25f344f5d9bc75744fb880cf3008c5b578b0a03d
forceTlight <gamebox27@naver.com>
2022-06-03 12:59:06 +0900

    [FEAT] /부활단축 추가 커맨드 적용, GamemodeChangedEvent & PlayerJoinEvent 추가 및 처리

--- DelayRevive/src/main/java/neo/command/Commands.java
+++ DelayRevive/src/main/java/neo/command/Commands.java
@@ -1,18 +1,51 @@
 package neo.command;
 
+import neo.data.DataManager;
+import neo.main.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class Commands implements CommandExecutor {
+    DataManager data = Main.getData();
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if(!(sender instanceof Player)) {
             sender.sendMessage("cmd에서는 이 작업을 수행하지 못합니다.");
             return true;
         }else {
-
+            Player p = (Player)sender;
+            if(label.equals("부활단축")){
+                switch(args.length){
+                    case 3:
+                        String arg = args[0];
+                        if(arg.equals("추가")){
+                            String nickName = args[1];
+                            int count = Integer.parseInt(args[2]);
+                            boolean check = false;
+                            OfflinePlayer[] players = Bukkit.getServer().getOfflinePlayers();
+                            for(OfflinePlayer op: players){
+                                if(nickName.equals(op.getName())){
+                                    check = true;
+                                    break;
+                                }
+                            }
+                            if(!check){ // 유저가 서버에 존재하지 않을 때
+                                p.sendMessage(ChatColor.RED + "존재하지 않는 유저입니다. 다시 확인해주세요");
+                                return true;
+                            }
+                            // data.yml에 추가
+                            data.getFile().set("reviveShorten."+nickName+".count", count);
+                            data.saveConfig();
+                            p.sendMessage(ChatColor.GREEN + nickName +"의 부활 단축 횟수가 " + count +"로 지정되었습니다.");
+                            return true;
+                        }
+                }
+            }
         }
         return true;
     }
--- DelayRevive/src/main/java/neo/event/EventListener.java
+++ DelayRevive/src/main/java/neo/event/EventListener.java
@@ -4,11 +4,13 @@ import neo.data.DataManager;
 import neo.main.Main;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.server.TabCompleteEvent;
 
 public class EventListener implements Listener {
@@ -23,12 +25,41 @@ public class EventListener implements Listener {
         p.setGameMode(GameMode.SPECTATOR);
         p.spigot().respawn();
         p.sendTitle(ChatColor.RED + "" + ChatColor.BOLD+ "당신은 죽었습니다 !", "한시간 후에 자동으로 부활합니다",10, 60, 20);
-        int reviveTime = data.getFile().getInt(name+ ".revive.lefttime");
-        data.getFile().set("name.revive.lefttime", System.currentTimeMillis() + reviveTime);
+        int reviveTime = data.getFile().getInt(name+ ".revive.time"); // 부활시간 가져오기
+        data.getFile().set(name+".revive.time", System.currentTimeMillis() + reviveTime);
         data.saveConfig();
     }
+    /*
+        서바이벌 모드 변경 이벤트 처리
+     */
     @EventHandler
-    public void onGameModeChangedEvent(PlayerGameModeChangeEvent e){
-
+    public void onGameModeChangedEvent(PlayerGameModeChangeEvent e) {
+        if (e.getPlayer().getGameMode() == GameMode.SURVIVAL) {
+            Player p = e.getPlayer();
+            String name = p.getName();
+            if (data.getFile().contains(name + ".revive.time")) {
+                int time = data.getFile().getInt(name + ".revive.time");
+                if (time >= System.currentTimeMillis()) { // 부활 딜레이가 안지났으면 타이틀 메시지 출력
+                    p.sendTitle(ChatColor.RED + "" + ChatColor.BOLD+ "부활 !", "",10, 60, 20);
+                }
+            }
+        }
+    }
+    /*
+        관전자 모드에서 1시간이 지나고 들어올 때 서바이벌 모드로 변경 및 스폰 장소 이동
+     */
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent e){
+        Player p = e.getPlayer();
+        String name = p.getName();
+        if(data.getFile().contains(name+".revive.time")){
+            int time = data.getFile().getInt(name+".revive.time");
+            if(time <= System.currentTimeMillis()){ // 부활 딜레이가 지났으면
+                data.getFile().set(name+".revive.time", null);
+                Location loc = p.getWorld().getSpawnLocation();
+                p.teleport(loc); // 스폰 장소로 이동
+                p.setGameMode(GameMode.SURVIVAL);
+            }
+        }
     }
 }
