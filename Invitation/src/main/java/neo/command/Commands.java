ac618cab2c6a3b0abfba9eb01ca637f75a4ff5f6
forceTlight <gamebox27@naver.com>
2022-06-05 21:52:29 +0900

    [FIX] 초대권 사용 NullPointer 오류 해결

--- Invitation/src/main/java/neo/command/Commands.java
+++ Invitation/src/main/java/neo/command/Commands.java
@@ -17,51 +17,54 @@ import java.util.List;
 public class Commands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(!(sender instanceof Player)) {
+        if (!(sender instanceof Player)) {
             sender.sendMessage("cmd에서는 이 작업을 수행하지 못합니다.");
             return true;
-        }else {
-            Player p = (Player)sender;
-            switch(args.length){
+        } else {
+            Player p = (Player) sender;
+            switch (args.length) {
                 case 2:
-                    if(p.isOp()){
-                        // 초대권 생성
-                        if(args[0].equals("생성")){
-                            if(p.getInventory().firstEmpty() == -1){
+                    // 초대권 생성
+                    if (args[0].equals("생성")) {
+                        if (p.isOp()) {
+                            if (p.getInventory().firstEmpty() == -1) {
                                 p.sendMessage(ChatColor.RED + "인벤토리 슬롯을 비워주세요!");
                                 return true;
                             }
                             int count = Integer.parseInt(args[1]);
                             ItemStack invitation = createInvitation(count);
                             p.getInventory().addItem(invitation);
+                            p.sendMessage(ChatColor.GREEN + "초대권을 발급 받았습니다!");
                             return true;
+                        } else {
+                            p.sendMessage(ChatColor.RED + "OP만 사용 가능합니다.");
                         }
-                    }else{
-                        p.sendMessage(ChatColor.RED + "OP만 사용 가능합니다.");
                     }
                     // 초대권 사용
-                    if(args[0].equals("사용")){
+                    if (args[0].equals("사용")) {
                         ItemStack invitation = p.getInventory().getItemInMainHand();
                         int count = invitation.getAmount();
                         ItemMeta itemMeta = invitation.getItemMeta();
-                        if(itemMeta.getDisplayName().equals(ChatColor.GOLD + "초대권")){
+                        if (itemMeta != null && itemMeta.getDisplayName().equals(ChatColor.GOLD + "초대권")) {
                             boolean check = false;
                             String nickName = args[1];
-                            for(OfflinePlayer offlinePlayer: Bukkit.getOfflinePlayers()){
-                                if(offlinePlayer.getPlayer().getName().equals(nickName)){
-                                    check = true;
-                                    offlinePlayer.setWhitelisted(true);
+                            for (OfflinePlayer offlinePlayer : Bukkit.getOfflinePlayers()) {
+                                if(offlinePlayer.getName() != null) {
+                                    if (offlinePlayer.getName().equals(nickName)) {
+                                        check = true;
+                                        offlinePlayer.setWhitelisted(true);
+                                    }
                                 }
                             }
-                            if(!check){
+                            if (!check) {
                                 p.sendMessage(ChatColor.RED + "존재하지 않는 유저 닉네임입니다.");
                                 return true;
                             }
                             p.getInventory().remove(invitation);
-                            p.getInventory().setItemInMainHand(createInvitation(count-1));
+                            p.getInventory().setItemInMainHand(createInvitation(count - 1));
                             p.sendMessage(ChatColor.GOLD + nickName + ChatColor.GREEN + "님이 화이트 리스트로 추가되셨습니다!");
                             return true;
-                        }else{
+                        } else {
                             p.sendMessage(ChatColor.RED + "초대권을 들고 명령어를 입력해주세요!");
                             return true;
                         }
@@ -70,9 +73,10 @@ public class Commands implements CommandExecutor {
         }
         return true;
     }
+
     // 초대권 생성
-    public ItemStack createInvitation(int count){
-        ItemStack invitation =  new ItemStack(Material.PAPER, count);
+    public ItemStack createInvitation(int count) {
+        ItemStack invitation = new ItemStack(Material.PAPER, count);
         ItemMeta itemMeta = invitation.getItemMeta();
         itemMeta.setDisplayName(ChatColor.GOLD + "초대권");
         List<String> lore = new ArrayList<>();
