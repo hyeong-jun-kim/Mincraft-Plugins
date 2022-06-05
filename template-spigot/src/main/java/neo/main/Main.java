a347a09071584a858c3b9a3301839ce8cfd7ed7e
forceTlight <gamebox27@naver.com>
2022-06-05 18:53:04 +0900

    Create template-spigot

---
+++ template-spigot/src/main/java/neo/main/Main.java
@@ -0,0 +1,27 @@
+package neo.main;
+import neo.command.Commands;
+import neo.data.DataManager;
+import neo.event.EventListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    public static DataManager data;
+
+    @Override
+    public void onEnable() {
+        data = new DataManager(this);
+//        getServer().getPluginManager().registerEvents(new EventListener(), this);
+        getCommand("커맨드").setExecutor(new Commands());
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public static DataManager getData() {
+        return data;
+    }
+
+}
+
