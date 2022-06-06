d9dd726ca6140072027b2be4cde17f372b905d78
forceTlight <gamebox27@naver.com>
2022-06-06 18:53:29 +0900

    [FIX] PlayerJoinEvent 부활 안되는 버그 수정

--- DelayRevive/src/main/java/neo/event/EventListener.java
+++ DelayRevive/src/main/java/neo/event/EventListener.java
@@ -22,9 +22,11 @@ import org.bukkit.scheduler.BukkitScheduler;
 import java.util.HashMap;
 
 public class EventListener implements Listener {
+    // data.yml 부활시간
+    private final long reviveTime = data.getFile().getLong("revive.time"); // 부활시간 가져오기
     // 부활시간 1시간, 단축시간 1분
     private final int SHORT_REVIVE_TIME = 60;
-    private final int REVIVE_TIME = 3600;
+    private final long REVIVE_TIME = reviveTime/1000L;
     // 부활 중복 방지 해쉬 맵
     HashMap<String, Integer> reviveTask = new HashMap<>();
     Main plugin;
@@ -62,7 +64,6 @@ public class EventListener implements Listener {
             playerRevive(p, 20 * SHORT_REVIVE_TIME);
         }else{
             p.sendTitle(ChatColor.RED + "" + ChatColor.BOLD+ "당신은 죽었습니다 !", "한시간 후에 자동으로 부활합니다",10, 100, 20);
-            long reviveTime = data.getFile().getLong("revive.time"); // 부활시간 가져오기
             data.getFile().set(name+".revive.time", System.currentTimeMillis() + reviveTime);
             data.saveConfig();
             // 1시간 뒤에 부활 (타이머 이용)
@@ -98,9 +99,14 @@ public class EventListener implements Listener {
             long time = data.getFile().getLong(name+".revive.time");
             if(time <= System.currentTimeMillis()){ // 부활 딜레이가 지났으면
                 data.getFile().set(name+".revive.time", null);
+                data.saveConfig();
                 Location loc = getSpawnLocation(p);
                 p.teleport(loc); // 스폰 장소로 이동
                 p.setGameMode(GameMode.SURVIVAL);
+                p.sendTitle(ChatColor.RED + "" + ChatColor.BOLD+ "부활 !", "",10, 100, 20);
+                if(reviveTask.containsKey(p.getName())){
+                    reviveTask.remove(p.getName());
+                }
             }else{ // 부활 딜레이가 안지났으면 남은 시간 틱으로 변환 후 부활
                 time = (time - System.currentTimeMillis()) / 1000 * 20;
                 playerRevive(p, (int)time);
@@ -117,7 +123,7 @@ public class EventListener implements Listener {
         }
     }
     // 부활
-    private void playerRevive(Player p, int tick){
+    private void playerRevive(Player p, long tick){
         BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
         // 중복된 부활 취소
         if(reviveTask.containsKey(p.getName())){
@@ -127,14 +133,16 @@ public class EventListener implements Listener {
         }
         int id = scheduler.scheduleSyncDelayedTask(plugin, new Runnable(){
             @Override
-            public void run(){
-                Location loc = getSpawnLocation(p);
-                p.spigot().respawn();
-                p.teleport(loc);
-                p.setGameMode(GameMode.SURVIVAL);
-                p.sendTitle(ChatColor.RED + "" + ChatColor.BOLD+ "부활 !", "",10, 100, 20);
-                if(reviveTask.containsKey(p.getName())){
-                    reviveTask.remove(p.getName());
+            public void run() {
+                if (p.isOnline()) {
+                    Location loc = getSpawnLocation(p);
+                    p.spigot().respawn();
+                    p.teleport(loc);
+                    p.setGameMode(GameMode.SURVIVAL);
+                    p.sendTitle(ChatColor.RED + "" + ChatColor.BOLD + "부활 !", "", 10, 100, 20);
+                    if (reviveTask.containsKey(p.getName())) {
+                        reviveTask.remove(p.getName());
+                    }
                 }
             }
         }, tick);
