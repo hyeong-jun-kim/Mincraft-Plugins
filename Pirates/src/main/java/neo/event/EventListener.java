5b5adb2f93bfbfb3eb4eb9e4d7895489b56c7d81
Neo <gamebox27@naver.com>
2022-08-06 22:07:15 +0900

    [INIT] - 해적단 플러그인

---
+++ Pirates/src/main/java/neo/event/EventListener.java
@@ -0,0 +1,9 @@
+package neo.event;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.TabCompleteEvent;
+
+public class EventListener implements Listener {
+
+}
