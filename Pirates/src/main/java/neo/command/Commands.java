2a1f83b21cd3117b7010ac98c32912849517c60c
Neo <gamebox27@naver.com>
2022-08-13 21:56:48 +0900

    [FEAT] /해적단 설명 커맨드 추가

--- Pirates/src/main/java/neo/command/Commands.java
+++ Pirates/src/main/java/neo/command/Commands.java
@@ -3,6 +3,7 @@ package neo.command;
 import neo.feature.Captain;
 import neo.feature.Sailor;
 import neo.main.Main;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,6 +23,15 @@ public class Commands implements CommandExecutor {
             if (label.equals("해적단")) {
                 Player p = (Player) sender;
                 switch (args.length) {
+                    case 0:
+                        p.sendMessage(ChatColor.YELLOW + "/해적단 초대 (닉네임)" + ChatColor.WHITE + "- 자신의 영토에 (닉네임)을 초대합니다.\n" +
+                                ChatColor.YELLOW + "/해적단 이름 (이름) " + ChatColor.WHITE + "- 해적단 이름을 설정합니다. ( 최대 7글자 )\n" +
+                                ChatColor.YELLOW + "/해적단 추방 (닉네임) " + ChatColor.WHITE + "- (닉네임)을 영토에서 추방합니다.\n" +
+                                ChatColor.YELLOW + "/해적단 정보 " + ChatColor.WHITE + "- 자신의 해적단 인원 목록을 출력합니다.\n" +
+                                ChatColor.YELLOW + "/해적단 탈퇴 " + ChatColor.WHITE + "- 해적단에서 탈퇴합니다.\n" +
+                                ChatColor.YELLOW + "/해적단 목록 " + ChatColor.WHITE + "- 해적단 목록을 출력합니다.\n" +
+                                ChatColor.YELLOW + "/해적단 채팅 " + ChatColor.WHITE + "- 해적단 채팅을 on/off 합니다.");
+                        break;
                     case 1:
                         Sailor sailor = new Sailor(main, p);
                         if(args[0].equals("정보")){
